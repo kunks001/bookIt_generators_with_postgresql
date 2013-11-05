2c0271baef1a45615b60313d3aa03a38a290aa7e
Margo <margo@margonline.co.uk>
2013-11-05 14:32:28 +0000

    changed templates for model_generators

--- lib/generators/booking_model_generator.rb
+++ lib/generators/booking_model_generator.rb
@@ -1,5 +1,5 @@
 class BookingModelGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
+  source_root File.expand_path('../templates/models', __FILE__)
 
   def generate_booking_model
     copy_file "bookable.rb", "app/models/concerns/bookable.rb"
