0f4155a7c7ab5844790202125ec2613094b7b93d
Margo <margo@margonline.co.uk>
2013-11-05 14:54:01 +0000

    remove initializer folder

--- lib/generators/initializer/initializer_generator.rb
+++
@@ -1,8 +0,0 @@
-class InitializerGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
-
-  def generate_datetime_initializer
-    copy_file "datetime.rb", "config/initializers/datetime.rb"
-  end
-  
-end
